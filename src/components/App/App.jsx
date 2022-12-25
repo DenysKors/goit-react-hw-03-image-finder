b24b398894fd5ebc245b0f88e9a6dca9ea24806f
DenysKors <105918404+DenysKors@users.noreply.github.com>
2022-12-25 14:27:54 +0200

    Searchbar done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
+import { ToastContainer } from 'react-toastify';
 import { Searchbar } from 'components/Searchbar/Searchbar';
 
 export class App extends Component {
   state = {
-    id: '',
-    webformatURL: '',
-    largeImageURL: '',
+    query: '',
+  };
+
+  searchQuery = query => {
+    console.log(query);
+    const normalizedQuery = query.toLowerCase().trim();
+    this.setState({ query: normalizedQuery });
+    console.log(this.state);
   };
 
   render() {
     return (
       <>
-        <Searchbar />
+        <Searchbar onSubmit={this.searchQuery} />
+        <ToastContainer position="top-left" theme="dark" autoClose={3000} />
       </>
     );
   }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { toast } from 'react-toastify';
 import { Head, Form, Button, Search, Input } from './Searchbar.styled';
+import 'react-toastify/dist/ReactToastify.css';
 
 export class Searchbar extends Component {
   state = {
@@ -10,10 +12,24 @@ export class Searchbar extends Component {
     const { value } = evt.target;
     this.setState({ query: value });
   };
+
+  onFormSubmit = evt => {
+    evt.preventDefault();
+    if (this.state.query.trim() === '') {
+      return toast.info('Enter request to search');
+    }
+    this.props.onSubmit(this.state.query);
+    this.resetForm();
+  };
+
+  resetForm = () => {
+    this.setState({ query: '' });
+  };
+
   render() {
     return (
       <Head>
-        <Form>
+        <Form onSubmit={this.onFormSubmit}>
           <Button type="submit">
             <Search>Search</Search>
           </Button>
