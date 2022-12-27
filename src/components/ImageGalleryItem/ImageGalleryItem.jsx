ad4c1f648716078b3fd4dbfe547d76bdc61d9153
DenysKors <105918404+DenysKors@users.noreply.github.com>
2022-12-27 14:07:40 +0200

    ModalWindow styles

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -2,7 +2,7 @@ import { GalleryItem } from './ImageGalleryItem.styled';
 import { GalleryImage } from './ImageGalleryItem.styled';
 
 export const ImageGalleryItem = ({ responseData }) => {
-  return responseData.map(({ id, webformatURL, tags }) => (
+  return responseData.map(({ id, webformatURL, largeImageURL, tags }) => (
     <GalleryItem key={id}>
       <GalleryImage src={webformatURL} alt={tags} />
     </GalleryItem>
---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,11 @@
+import { ModalWindow, ModalOverlay } from './Modal.styled';
+
+export const Modal = () => {
+  return (
+    <ModalOverlay>
+      <ModalWindow>
+        <img src="" alt="" />
+      </ModalWindow>
+    </ModalOverlay>
+  );
+};
