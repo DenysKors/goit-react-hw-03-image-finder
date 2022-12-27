42ddcd240faa2c73280e563b6feff12035bc0bee
DenysKors <105918404+DenysKors@users.noreply.github.com>
2022-12-27 13:14:57 +0200

    Add Load more img

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -17,7 +17,7 @@ export class ImageGallery extends Component {
 
   async componentDidUpdate(prevProps, prevState) {
     if (prevProps.query !== this.props.query) {
-      this.setState({ page: 1, loading: true });
+      this.setState({ responseData: [], page: 1, loading: true });
       try {
         const responceQuery = await fetch(
           `https://pixabay.com/api/?key=${KEY}&q=${this.props.query}&page=1&image_type=photo&orientation=horizontal&page=${this.state.page}&per_page=12`
@@ -29,14 +29,19 @@ export class ImageGallery extends Component {
       } finally {
         this.setState({ loading: false });
       }
-    } else if (prevState.page !== this.state.page) {
+    } else if (
+      prevState.page !== this.state.page &&
+      prevProps.query === this.props.query
+    ) {
       this.setState({ loading: true });
       try {
         const responceQuery = await fetch(
           `https://pixabay.com/api/?key=${KEY}&q=${this.props.query}&page=1&image_type=photo&orientation=horizontal&page=${this.state.page}&per_page=12`
         );
         const data = await responceQuery.json();
-        this.setState({ responseData: data.hits });
+        this.setState(prevState => ({
+          responseData: [...prevState.responseData, ...data.hits],
+        }));
       } catch (error) {
         toast.warn('Something weird happend. Please try your request again');
       } finally {
